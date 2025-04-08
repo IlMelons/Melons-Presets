b459a8a27d5abd580ae44477d525a85d57ca75eb
IlMelons <melonss2002@gmail.com>
2025-04-08 10:01:05 +0200

    Add files via upload

---
+++ blips/k4mb1.lua
@@ -0,0 +1,12 @@
+---@diagnostic disable: exp-in-action, unknown-symbol
+---@description K4mb1 Maps Blips Presets [https://k4mb1maps.com/]
+
+["diamond-casino"] = {
+    label = "Diamond Casino",
+    coords = vec3(924.25, 47.39, 81.00),
+    display = 2,
+    sprite = 679,
+    color = 0,
+    alpha = 255,
+    scale = 0.8,
+},
---
+++ blips/nteam.lua
@@ -0,0 +1,3 @@
+---@diagnostic disable: exp-in-action, unknown-symbol
+---@description NTeam Blips Presets [https://nteamdev.tebex.io/]
+
